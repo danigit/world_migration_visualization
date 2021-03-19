2ca938bb6577c3ac605a96673816593ffb76fd9e
danigit <ds.acconto@gmail.com>
2021-03-19 16:39:45 +0100

    GENERIC UPDATES
    - implemented the imigration average age statistic
    - implemented the funcition that transform the numbers in "k", "M" format
    - updated the graphical elements for the 100 browser zoom
    - updated the values of the sliders
    - implemented the listeners for the slider and buttons in country statistics
    - fixed the radio-button directive

--- app/js/controllers/country_controller.js
+++ app/js/controllers/country_controller.js
@@ -33,8 +33,8 @@
 
         // variable that holds the slider values
         $scope.sliderCountry = {
-            minValue: 1,
-            maxValue: 5,
+            minValue: 1990,
+            maxValue: 2019,
             options: {
                 floor: 4,
                 ceil: 9,
@@ -42,12 +42,6 @@
                 stepsArray: dataService.sliderYears,
             },
         };
-
-        // getting the min and max year in the slider
-        let sliderMin = dataService.sliderYears.find((sliderYear) => sliderYear.value == $scope.sliderCountry.minValue).legend;
-        let sliderMax = dataService.sliderYears.find((sliderYear) => sliderYear.value == $scope.sliderCountry.maxValue).legend;
-        // getting the years selected in the slider
-        let consideredYears = [1990, 1995, 2000, 2005, 2010, 2015, 2019].filter((year) => year >= +sliderMin && year <= +sliderMax);
 
         /**
          * Function that returns the column postfix given the gender
@@ -71,53 +65,76 @@
             return selectedGenderColumn;
         };
 
-        // getting the total migrants by origin and destination
-        dataService.getTotMigrantsByOriginAndDestination($scope.selectedCountryController, sliderMin, sliderMax).then((data) => {
-            $scope.countryStatisticsValues.totalImmigrations = "" + data;
-            $scope.$apply();
+        // getting the min and max year in the slider
+        let sliderMin = 1900;
+        let sliderMax = 2019;
+
+        // getting the years selected in the slider
+        let consideredYears = [1990, 1995, 2000, 2005, 2010, 2015, 2019].filter((year) => year >= +sliderMin && year <= +sliderMax);
+
+        $scope.$on("slideEnded", () => {
+            sliderMin = $scope.sliderCountry.minValue;
+            sliderMax = $scope.sliderCountry.maxValue;
+            updateStatistics();
         });
 
-        // getting the total population by age and sex
-        dataService
-            .getTotPopulationByAgeAndSex(
-                $scope.selectedCountryController,
-                sliderMin,
-                sliderMax,
-                getSelectedGenderColumn($scope.genreFilterValue, "Total")
-            )
-            .then((data) => {
-                $scope.countryStatisticsValues.totalPopulation = "" + data;
+        let updateStatistics = () => {
+            // getting the total migrants by origin and destination
+            dataService.getTotMigrantsByOriginAndDestination($scope.selectedCountryController, sliderMin, sliderMax).then((data) => {
+                $scope.countryStatisticsValues.totalImmigrations = "" + transformNumberFormat(data);
                 $scope.$apply();
             });
 
-        // getting the migrants as percentage of population
-        dataService
-            .getMigrantsAsPercentageOfPopulationByAgeAndSex(
-                $scope.selectedCountryController,
-                sliderMin,
-                sliderMax,
-                getSelectedGenderColumn($scope.genreFilterValue, "Total")
-            )
-            .then((data) => {
-                $scope.countryStatisticsValues.immigrationVsPopulation = "" + data.toFixed(2);
-                $scope.$apply();
-            });
+            // getting the total population by age and sex
+            dataService
+                .getTotPopulationByAgeAndSex(
+                    $scope.selectedCountryController,
+                    sliderMin,
+                    sliderMax,
+                    getSelectedGenderColumn($scope.genreFilterValue, "Total")
+                )
+                .then((data) => {
+                    $scope.countryStatisticsValues.totalPopulation = "" + transformNumberFormat(data);
+                    $scope.$apply();
+                });
 
-        // getting the immigration average age
-        // TODO
+            // getting the migrants as percentage of population
+            dataService
+                .getMigrantsAsPercentageOfPopulationByAgeAndSex(
+                    $scope.selectedCountryController,
+                    sliderMin,
+                    sliderMax,
+                    getSelectedGenderColumn($scope.genreFilterValue, "Total")
+                )
+                .then((data) => {
+                    $scope.countryStatisticsValues.immigrationVsPopulation = "" + transformNumberFormat(data);
+                    $scope.$apply();
+                });
 
-        // getting the estimated refugees
-        dataService
-            .getEstimatedRefugees(
-                $scope.selectedCountryController,
-                consideredYears,
-                getSelectedGenderColumn($scope.genreFilterValue, "_est")
-            )
-            .then((data) => {
-                $scope.countryStatisticsValues.refugeeVsImmigration = "" + data;
-                $scope.$apply();
-                console.log("getEstimatedRefugees: " + data);
-            });
+            // getting the immigration average ag
+            dataService
+                .getImmigrationAverageAge(
+                    $scope.selectedCountryController,
+                    sliderMin,
+                    sliderMax,
+                    getSelectedGenderColumn($scope.genreFilterValue, "")
+                )
+                .then((data) => {
+                    $scope.countryStatisticsValues.immigrationAverageAge = "" + transformNumberFormat(data);
+                });
+
+            // getting the estimated refugees
+            dataService
+                .getEstimatedRefugees(
+                    $scope.selectedCountryController,
+                    consideredYears,
+                    getSelectedGenderColumn($scope.genreFilterValue, "_est")
+                )
+                .then((data) => {
+                    $scope.countryStatisticsValues.refugeeVsImmigration = "" + transformNumberFormat(data);
+                    $scope.$apply();
+                });
+        };
 
         /**
          * Function that handles the click on the genre radio group filter in the menu
@@ -125,6 +142,7 @@
          */
         $scope.handleGenreClick = function (value) {
             $scope.genreFilterValue = value;
+            updateStatistics();
         };
 
         /**
@@ -153,89 +171,5 @@
             $scope.selectedTopCountry = value;
             $scope.selectedCountryController = value;
         };
-
-        // dataService.totMigrByOriginDest.then(function (data) {
-        //     $scope.selectedCountryInwardMigrData = data.filter(
-        //         (countryData) =>
-        //             countryData["Destination"] ==
-        //                 $scope.selectedCountryController &&
-        //             countryData["Year"] >= sliderMin &&
-        //             countryData["Year"] <= sliderMax
-        //     );
-        //     console.log("data ", $scope.selectedCountryInwardMigrData);
-        //     $scope.AverageInwardMigrants =
-        //         $scope.selectedCountryInwardMigrData.reduce(
-        //             (sum, curr) => sum + +curr.Total,
-        //             0
-        //         ) / $scope.selectedCountryInwardMigrData.length;
-        //     console.log("Average migrants ", $scope.AverageInwardMigrants);
-        // });
-
-        // dataService.totPopulationByAgeSex.then(function (data) {
-        //     $scope.selectedCountryTotPopulationData = data.filter(
-        //         (countryData) =>
-        //             countryData["Destination"] == $scope.selectedCountry &&
-        //             countryData["Year"] >= sliderMin &&
-        //             countryData["Year"] <= sliderMax
-        //     );
-
-        //     $scope.AveragePopulation =
-        //         ($scope.selectedCountryTotPopulationData.reduce((sum, curr) => sum + +curr[selectedGender], 0) /
-        //             $scope.selectedCountryTotPopulationData.length) *
-        //         1000;
-        //     console.log("Average population ", $scope.AveragePopulation);
-        // });
-
-        // var selectedGender = "";
-        // if ($scope.genreFilterValue == "menu-all") {
-        //     selectedGender = "Total_(mf)";
-        // } else if ($scope.genreFilterValue == "menu-male") {
-        //     selectedGender = "Total_(m)";
-        // } else if ($scope.genreFilterValue == "menu-female") {
-        //     selectedGender = "Total_(f)";
-        // }
-
-        // dataService.migrAsPercOfPopulationAgeSex.then(function (data) {
-        //     $scope.selectedCountryMigrPerc = data.filter(
-        //         (countryData) =>
-        //             countryData["Destination"] == $scope.selectedCountry &&
-        //             countryData["Year"] >= sliderMin &&
-        //             countryData["Year"] <= sliderMax
-        //     );
-
-        //     $scope.averagePercOfMigr =
-        //         $scope.selectedCountryMigrPerc.reduce((sum, curr) => sum + +curr[selectedGender], 0) /
-        //         $scope.selectedCountryMigrPerc.length;
-        //     console.log("Average percentage of migrants ", $scope.averagePercOfMigr);
-        // });
-
-        // dataService.estimatedRefugees.then(function (data) {
-        //     let selected = "";
-        //     if ($scope.genreFilterValue == "menu-all") {
-        //         selected = "_est_(mf)";
-        //     } else if ($scope.genreFilterValue == "menu-male") {
-        //         selected = "_est_(m)";
-        //     } else if ($scope.genreFilterValue == "menu-female") {
-        //         selected = "_est_(f)";
-        //     }
-
-        //     $scope.selectedCountryEstRefugees = data.filter(
-        //         (countryData) => countryData["Destination"] == $scope.selectedCountryController
-        //     );
-        //     var leftLimit = +sliderMin;
-        //     var rightLimit = +sliderMax;
-        //     $scope.averageEstRefugees = 0;
-        //     let boundaries = [1990, 1995, 2000, 2005, 2010, 2015, 2019];
-        //     var numYears = 0;
-        //     boundaries.forEach((year) => {
-        //         if (year >= leftLimit && year <= rightLimit) {
-        //             $scope.averageEstRefugees += +$scope.selectedCountryEstRefugees[0]["" + year + selected];
-        //             numYears = numYears + 1;
-        //         }
-        //     });
-
-        //     $scope.averageEstRefugees = $scope.averageEstRefugees / numYears;
-        //     console.log("Average estimated of refugees", $scope.averageEstRefugees);
-        // });
     }
 })();
