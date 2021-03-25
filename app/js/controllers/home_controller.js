1057a310e41a29e1f016714dfb4c9442dafab4d1
danigit <ds.acconto@gmail.com>
2021-03-25 01:04:05 +0100

    MAP
    - implemented the zoom in the home section map
    - implemented the zoom in the country section map

--- app/js/controllers/home_controller.js
+++ app/js/controllers/home_controller.js
@@ -17,29 +17,42 @@
             drawMap(data);
         });
 
-        let svgWidth;
-        let svgHeight;
-        let projection;
+        let svgGroup;
 
         let drawMap = (data) => {
             let mapContainer = d3.select("#map");
-            svgWidth = mapContainer.node().getBoundingClientRect().width;
-            svgHeight = mapContainer.node().getBoundingClientRect().height;
-            projection = d3
-                .geoConicConformal()
-                .scale(175)
-                .translate([svgWidth / 2 - 100, svgHeight / 2 + 100]);
+            let svgWidth = mapContainer.node().getBoundingClientRect().width;
+            let svgHeight = mapContainer.node().getBoundingClientRect().height;
+            let projection = d3
+                .geoMercator()
+                .scale(170)
+                .translate([svgWidth / 2, svgHeight / 2]);
             let path = d3.geoPath().projection(projection);
 
             let svgMap = mapContainer.append("svg").attr("width", svgWidth).attr("height", svgHeight);
-            svgMap
+            svgGroup = svgMap.append("g");
+            svgGroup
                 .selectAll("path")
                 .data(topojson.feature(data, data.objects.countries).features)
                 .enter()
                 .append("path")
                 .attr("d", path)
                 .attr("class", "countries")
-                .attr("id", (d) => d.id);
+                .attr("id", (d) => d.id)
+                .on("click", (e, d) => {
+                    console.log(e);
+                    console.log(d);
+                });
+
+            svgMap.call(zoom);
+            svgMap.call(zoom.transform, () => d3.zoomIdentity.scale(1));
         };
+
+        let zoom = d3
+            .zoom()
+            .scaleExtent([1, 10])
+            .on("zoom", (event) => {
+                svgGroup.attr("transform", event.transform);
+            });
     }
 })();
