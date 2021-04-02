2a76126aa14a63f2916be79264498c4708e13107
matteoghirardelli <matteoghirardelli01@gmail.com>
2021-04-02 22:56:20 +0200

    modified util.js

--- app/js/util/util.js
+++ app/js/util/util.js
@@ -24,7 +24,7 @@ function transformNumberFormat(number, order = false, decimals = 2) {
             { value: 1, symbol: "" },
             { value: 1e3, symbol: "k" },
             { value: 1e6, symbol: "M" },
-            { value: 1e9, symbol: "B" },
+            { value: 1e9, symbol: "G" },
             { value: 1e12, symbol: "T" },
             { value: 1e15, symbol: "P" },
             { value: 1e18, symbol: "E" },
